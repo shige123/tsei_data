d80732ee7e52b3353141b8be466d2cbcc50a66b3
sigenobu <kikyu627@gmail.com>
2016-11-09 12:56:54 +0900

    test

--- comvertdata4.py
+++ comvertdata4.py
@@ -1,10 +1,16 @@
+# -*- coding: utf-8 -*-
+import codecs
 import csv
 import datetime
 import os.path
 import sys
 
-def get_file_name(fname, day):
+sys.stdout = codecs.getwriter('utf_8')(sys.stdout)
+
+def get_file_name(day):
     one_day = datetime.timedelta(days=1)
+    print one_day
+    print day
     while 1:
         day = day - one_day
         fname = day.strftime("pick_stocks_%Y-%m-%d.csv")
@@ -12,16 +18,87 @@ def get_file_name(fname, day):
             break
         else:
             continue
-    return fname
+    return fname, day
+
+fout = open('data.tsei','wb')
 
 day = datetime.date(2016, 11, 2)
 
-fname1 = ""
+fname1,day = get_file_name(day)
+f1 = open(fname1,"rb")
+reader1 = csv.reader(f1)
+header = next(reader1)
+com_name = []
+count = 0
+for row in reader1:
+    com_name.append(row[0])
+    count+=1
+f1.close()
 
-fname1 = get_file_name(fname1, day)
+fname2,day = get_file_name(day)
+print fname2
 
-print fname1
-print day
-fname1 = get_file_name(fname1, day)
+fname3,day = get_file_name(day)
+print fname3
 
-print fname1
+bary = bytearray(['t','s','e','i'])
+bary.extend([0x7,0xC2])
+bary.extend([0,1])
+
+for date in range(0,30):
+    f1 = open(fname1,"rb")
+    reader1 = csv.reader(f1)
+    header = next(reader1)
+
+    f2 = open(fname2,"rb")
+    reader2 = csv.reader(f2)
+    header = next(reader2)
+
+    f3 = open(fname3,"rb")
+    reader3 = csv.reader(f3)
+    header = next(reader3)
+    
+    row1 = next(reader1)
+    row2 = next(reader2)
+    row3 = next(reader3)
+    
+    for num in range(0,count-1):
+        if com_name[num] == row3[0] and com_name[num] == row2[0]:
+            print "true"
+            if (float(row3[1]) - float(row2[1])) > 0:
+                bary.append(1)
+            else:
+                bary.append(0)
+        else:
+            print "false"
+            bary.append(0)
+            
+        if com_name[num] == row1[0]:
+            if 0.05 < (float(row1[1]) - float(row2[1])/float(row2[1])):
+                bary.append(1)
+            else:
+                bary.extend(0)
+        else:
+            bary.append(0)
+                
+        if com_name[num] == row1[0]:
+            row1 = next(reader1)
+           # print "T"
+       # else:
+           # print "N"
+        if com_name[num] == row2[0]:
+            row2 = next(reader2)
+        if com_name[num] == row3[0]:
+            row3 = next(reader3)
+    f1.close()
+    f2.close()
+    f3.close()
+    fname1 = fname2
+    fname2 = fname3
+    fname3,day = get_file_name(day)
+                
+fout.write(bary)
+f1.close()
+f2.close()
+f3.close()
+fout.close()
